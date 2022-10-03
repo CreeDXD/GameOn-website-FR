b1e80662f7ec8fe51fa2cc3f0eee3903d0a28297
CreeDXD <pclio92@gmail.com>
2022-10-03 15:36:50 +0200

    premier commit header et section remerciment

--- README.md
+++ README.md
@@ -3,3 +3,19 @@
 2. Il est conseillé d'utiliser VisualStudio Code et vous pouvez utiliser Docker, mais ce n'est pas obligatoire ;
 3. Il n'y a aucune dépendance ;
 4. Vous ne devez utiliser que du CSS personnalisé et du JavaScript pur, sans jQuery, Bootstrap ou autre librairie.
+
+
+//tests manuels:
+chrome:
+firefox:
+mobile:
+desktop:
+tester les fonctionnalités des boutons et entrées formulaire
+
+resultats:
+-boutons du header ne marche pas
+-mail n'est pas obligatoire
+-les conditions d'utilisations ne sont pas obligatoire
+-je peux mettre des chiffres dans la section prénom et nom
+-fermer ne marche pas
+<!-- -ligne 29 </div> en trop -->
--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,33 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelectorAll(".close");
+const submitBtn = document.querySelectorAll('.btn-submit');
+const submitBtnThanks = document.querySelectorAll('.btn-submit--thanks');
+const modalbgThanks = document.querySelector(".bground--thanks");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeBtn.forEach((btn) => btn.addEventListener("click", closeModal));
+submitBtn.forEach((btn) => btn.addEventListener("click", launchThanks));
+submitBtnThanks.forEach((btn) => btn.addEventListener("click", closethanks));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
-
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+  modalbgThanks.style.display = "none";
+}
+//close modal thanks
+function closethanks() {
+  modalbgThanks.style.display = "none";
+}
+// launch modal thanks
+function launchThanks(e) {
+  e.preventDefault();
+  modalbg.style.display = "none";
+  modalbgThanks.style.display = "flex";
+}
